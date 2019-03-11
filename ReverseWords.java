8b8e13c3a500b9b44a5e11d48c0000e184348fac
barros <barrospj@bc.edu>
2019-03-11 13:12:20 -0400

    code clean up

--- ReverseWords.java
+++ ReverseWords.java
@@ -4,15 +4,12 @@ import java.lang.StringBuilder;
 /*
 Given a string with multiple words and spaces represented as a character array.
 Write an in-place algorithm to reverse the order of words in the string.
-Example: convert ['p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e']
-to
-['p', 'r', 'a', 'c', 't', 'i', 'c', 'e', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'e', 'r', 'f', 'e', 'c', 't']
+Example: convert "perfect makes practice" to -> "practice makes perfect"
 */
 
 class ReverseWords {
   public static void main(String[] args) {
     String input = "perfect makes practice";
-    char[] input2 = {'p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e'};
     String reversed = reverseWords(input);
     System.out.println(reversed);
   }
